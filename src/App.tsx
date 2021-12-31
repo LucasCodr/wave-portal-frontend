6c3adcb9ca60e9f86773dadfc924fffc8fb23c92
Lucas <lucassouza876@outlook.com>
2021-12-31 11:22:21 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Container, Heading, Text, VStack} from "@chakra-ui/react";
+import {animated, useSpring} from "react-spring";
+import Moralis from 'moralis';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const styles = useSpring({
+		loop: true,
+		from: {rotateZ: 0},
+		to: {rotateZ: 360}
+	})
+
+	console.log(Moralis.applicationId);
+
+	return (
+		<Container mt={'40'}>
+			<VStack>
+				<animated.div style={{...styles}}>
+					<Text fontSize={'3xl'}>🤠</Text>
+				</animated.div>
+				<Heading>👋 Hey there!</Heading>
+				<Text textAlign={'center'}>I'm Lucas, one of the luxy.io builders :) <br/>Wave at me please :(</Text>
+				<Button w={'100%'}>Wave</Button>
+			</VStack>
+		</Container>
+	);
 }
 
 export default App;
